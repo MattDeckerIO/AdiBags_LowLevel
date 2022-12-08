ae2eea01e913bc917840d765d29fdd4747d40f25
Matt Decker <me@mattdecker.io>
2022-12-08 00:20:30 -0500

    Makes the addon compatible with 10.0.2

--- AdiBags_Lowlevel.lua
+++ AdiBags_Lowlevel.lua
@@ -5,23 +5,28 @@ All rights reserved.
 --]]
 
 local _, ns = ...
-
-local addon = LibStub('AceAddon-3.0'):GetAddon('AdiBags')
+local string_find = string.find
+local addon = LibStub("AceAddon-3.0"):GetAddon("AdiBags")
 local L = setmetatable({}, {__index = addon.L})
+local C_TooltipInfo_GetBagItem = C_TooltipInfo and C_TooltipInfo.GetBagItem
+local TooltipUtil_SurfaceArgs = TooltipUtil and TooltipUtil.SurfaceArgs
 
-local tooltip
 local function create()
-  local tip, leftside = CreateFrame("GameTooltip"), {}
-  for i = 1,6 do
+  local tip, leftTip, rightTip = CreateFrame("GameTooltip"), {}, {}
+  for x = 1,6 do
     local L,R = tip:CreateFontString(), tip:CreateFontString()
     L:SetFontObject(GameFontNormal)
     R:SetFontObject(GameFontNormal)
     tip:AddFontStrings(L,R)
-    leftside[i] = L
+    leftTip[x] = L
+    rightTip[x] = R
   end
-  tip.leftside = leftside
+  tip.leftTip = leftTip
+  tip.rightTip = rightTip
   return tip
 end
+
+local tooltip = tooltip or create()
 
 -- The filter itself
 
@@ -48,29 +53,55 @@ function setFilter:OnDisable()
   addon:UpdateFilters()
 end
 
-local setNames = {}
+-- Tooltip used for scanning.
+-- Let's keep this name for all scanner addons.
+local _SCANNER = "AVY_ScannerTooltip"
+local Scanner
+if not addon.WoW10 then
+	-- This is not needed on WoW10, since we can use C_TooltipInfo
+	Scanner = _G[_SCANNER] or CreateFrame("GameTooltip", _SCANNER, UIParent, "GameTooltipTemplate")
+end
+
+-- Cache of information objects,
+-- globally available so addons can share it.
+local Cache = AVY_ItemBindInfoCache or {}
+AVY_ItemBindInfoCache = Cache
 
 function setFilter:Filter(slotData)
-  tooltip = tooltip or create()
-  tooltip:SetOwner(UIParent,"ANCHOR_NONE")
-  tooltip:ClearLines()
 
-  if slotData.bag == BANK_CONTAINER then
-    tooltip:SetInventoryItem("player", BankButtonIDToInvSlotID(slotData.slot, nil))
-  else
-    tooltip:SetBagItem(slotData.bag, slotData.slot)
+	local bag, slot, quality, itemId = slotData.bag, slotData.slot, slotData.quality, slotData.itemId
+
+	local _, _, _, _, _, _, _, _, _, _, _, _, _, bindType, _, _, _ = GetItemInfo(itemId)
+
+  local level = self:GetItemCategory(bag, slot)
+  return level
+end
+
+
+function setFilter:GetItemCategory(bag, slot)
+	local category = nil
+
+  -- New API in WoW10 means we don't need an actual frame for the tooltip
+  -- https://wowpedia.fandom.com/wiki/Patch_10.0.2/API_changes#Tooltip_Changes
+  Scanner = C_TooltipInfo_GetBagItem(bag, slot)
+  -- The SurfaceArgs calls are required to assign values to the 'leftText' fields seen below.
+  TooltipUtil_SurfaceArgs(Scanner)
+  for _, line in ipairs(Scanner.lines) do
+    TooltipUtil_SurfaceArgs(line)
   end
+  for i = 2, 6 do
+    local line = Scanner.lines[i]
+    if (not line) then
+      break
+    end
 
-  for i = 1,6 do
-    local t = tooltip.leftside[i]:GetText()
-    if t ~= nil then
-      local m = t:match("^Item Level (%d+)$")
-      if self.db.profile.enable and m ~= nil and tonumber(m) < self.db.profile.level then
-        return "Low level"
-      end
+    local m = line.leftText:match("^Item Level (%d+)$")
+    if self.db.profile.enable and m ~= nil and tonumber(m) < self.db.profile.level then
+      return "Low level"
     end
   end
-  tooltip:Hide()
+
+	return category
 end
 
 function setFilter:GetOptions()
